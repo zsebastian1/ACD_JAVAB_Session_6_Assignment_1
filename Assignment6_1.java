819eb0d062a2b8b5c0a8a8bc2f44f9223863d07a
zsebastian1 <44416197+zsebastian1@users.noreply.github.com>
2019-08-14 23:07:03 -0400

    Add files via upload

---
+++ Assignment6_1.java
@@ -0,0 +1,19 @@
+package AssignmentSession6;
+
+public class Assignment6_1 {
+
+	public static void main(String[] args) {
+
+		int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] % 2 == 0) {
+				//continue;
+				System.out.println(arr[i] + " ");
+			}
+			//System.out.println(arr[i] + " ");
+
+		}
+	}
+
+}
